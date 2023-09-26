8abc2e8d545db6ab5fbff43a452a864911534dbd
Michel Pessoa <michelpessoa@gmail.com>
2023-09-26 20:47:50 -0300

    server ready

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -34,6 +35,12 @@ type CambioResposta struct {
 	ValorDolar string `json:"bid"`
 }
 
+type RequestError struct {
+	StatusCode int
+
+	Err error
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/cambio", CambioHandler)
@@ -42,38 +49,30 @@ func main() {
 }
 
 func CambioHandler(w http.ResponseWriter, r *http.Request) {
-	context, cancel := context.WithTimeout(r.Context(), time.Millisecond*210)
+	context, cancel := context.WithTimeout(r.Context(), time.Second)
 	defer cancel()
 
-	//time.Sleep(1 * time.Second) //Teste para o timeout
+	consulta, err := BuscaCambio(r)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	erroDB := SalvaConsultaCambio(consulta.Usdbrl, r)
+	if erroDB != nil {
+		fmt.Println(erroDB)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 	select {
 	case <-context.Done():
-		w.WriteHeader(http.StatusRequestTimeout)
+		fmt.Println("Fatal Error")
+		w.WriteHeader(http.StatusInternalServerError)
 	default:
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-
-		consulta, err := BuscaCambio()
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		context2, cancel := context.WithTimeout(r.Context(), time.Millisecond*10)
-		defer cancel()
-
-		time.Sleep(1 * time.Second) //Teste para o timeout
-
-		select {
-		case <-context2.Done():
-			w.WriteHeader(http.StatusGatewayTimeout)
-
-		default:
-			SalvaConsultaCambio(consulta.Usdbrl)
-
-		}
 
 		resposta := CambioResposta{consulta.Usdbrl.Bid}
 
@@ -82,38 +81,68 @@ func CambioHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func BuscaCambio() (*CambioConsulta, error) {
+func BuscaCambio(r *http.Request) (*CambioConsulta, error) {
+	context1, cancel := context.WithTimeout(r.Context(), time.Millisecond*200)
+	defer cancel()
 	resp, error := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
-	if error != nil {
-		return nil, error
-	}
-	defer resp.Body.Close()
-	body, error := io.ReadAll(resp.Body)
-	if error != nil {
-		return nil, error
+
+	//time.Sleep(3 * time.Second) //Teste para o timeout
+
+	select {
+	case <-context1.Done():
+		return nil, &RequestError{
+			StatusCode: 408,
+			Err:        errors.New(" timeout request"),
+		}
+	default:
+		if error != nil {
+			return nil, error
+		}
+		defer resp.Body.Close()
+		body, error := io.ReadAll(resp.Body)
+		if error != nil {
+			return nil, error
+		}
+
+		var c CambioConsulta
+		error = json.Unmarshal(body, &c)
+		if error != nil {
+			return nil, error
+		}
+		fmt.Println(c.Usdbrl.Bid)
+
+		return &c, nil
 	}
 
-	var c CambioConsulta
-	error = json.Unmarshal(body, &c)
-	if error != nil {
-		return nil, error
-	}
-	fmt.Println(c.Usdbrl.Bid)
-
-	return &c, nil
 }
 
-func SalvaConsultaCambio(consulta Usdbrl) error {
-	db, err := gorm.Open(sqlite.Open("cambio.db"), &gorm.Config{})
-	//db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-	db.AutoMigrate(&Usdbrl{})
-	err = db.Create(consulta).Error
-	if err != nil {
-		return err
-	}
-	return nil
+func SalvaConsultaCambio(consulta Usdbrl, r *http.Request) error {
+	context2, cancel := context.WithTimeout(r.Context(), time.Millisecond*10)
+	defer cancel()
 
+	//time.Sleep(3 * time.Second) //Teste para o timeout
+
+	select {
+	case <-context2.Done():
+		return &RequestError{
+			StatusCode: 408,
+			Err:        errors.New(" timeout transaction"),
+		}
+	default:
+		db, err := gorm.Open(sqlite.Open("cambio.db"), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+		db.AutoMigrate(&Usdbrl{})
+		err = db.Create(consulta).Error
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+}
+
+func (r *RequestError) Error() string {
+	return fmt.Sprintf("status %d: err %v", r.StatusCode, r.Err)
 }
